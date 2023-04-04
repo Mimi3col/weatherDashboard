4f29e44dd86a1fde78d3b58265632422a3124b01
Amanda Coleman <acoleman8097@floridapoly.edu>
2023-04-03 22:24:47 -0400

    making big changes

---
+++ component/DailyWeather.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+function DailyWeather({weatherData}) {
+    const [location, setLocation] = useState({
+        state:'',
+        city:''
+    })
+
+    if(weatherData == null){
+        return(
+            <div>
+                Searching For Data...
+            </div>
+        )
+    }
+
+    const weatherList = weatherData.map((element) =>(
+      <div
+        key={element.datetime}
+        className="weatherItem box-border border-slate-800 border-2  w-40  ">
+        <a className=" text-slate-300 ">{element.datetime}</a> <br></br>
+        <a className=" text-red-300">Daily High:</a> {element.max_temp}{" "}
+        <br></br>
+        <a className=" text-blue-300">Daily Low:</a> {element.min_temp} <br></br>
+        <a className=""> rain:{element.precip} in</a> <br></br>
+        <a className=""> wind: {(Number(element.wind_spd) * 2.23).toPrecision(2)} mph </a>
+      </div>
+    ));
+
+    return (
+        <div className="Weather_data grid grid-cols-6 grid-rows-5 gap-2">
+          {weatherList}
+        </div>
+    );
+    
+}
+
+export default DailyWeather; 
